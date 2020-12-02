6377ba01f41677d1aa987c3f3959c43f3a793b03
Chinmay Sonawane <69494946+chinmay29hub@users.noreply.github.com>
2020-12-02 09:13:23 +0530

    Update _translate.py

--- _translate.py
+++ _translate.py
@@ -1,8 +1,10 @@
 from translate import Translator
 
 translator = Translator(from_lang = 'Spanish', to_lang = 'english')
+print("Currently the program will convert from Spanish----->English\n\n")
+c = input("Enter the text to translate\n\n")
 
-result = translator.translate('Hola codificadora')
+result = translator.translate(c)
 
 
 print("The translated text is "+result)
